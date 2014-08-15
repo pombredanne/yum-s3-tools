4549b17cd622eeee56a28ed0337c8ae0b5182a0a
Michal Bicz <mbicz@twilio.com>
2014-08-15 00:15:43 -0700

    junk commit2

--- yum-s3-verify.py
+++ yum-s3-verify.py
@@ -19,44 +19,44 @@ from boto.s3.key import Key
 import os
 import sys
 import docopt
-
-def get_creds_from_env():
-    try:
-        access_key = os.environ['AWS_ACCESS_KEY']
-        secret_key = os.environ['AWS_SECRET_KEY']
-        region = os.environ['AWS_REGION']
-    except:
-        print "Environment not set properly!"
-        sys.exit(1)
-    return (access_key, secret_key)
+import tempfile
 
 
 class s3yumrepo(object):
     def __init__(self, auth, baseurl):
-        (access_key, secret_key) = get_creds_from_env()
+        if auth == 'env':
+            (access_key, secret_key) = self.env2creds()
+        if auth == 'iam':
         conn = boto.connect_s3(aws_access_key_id = access_key, aws_secret_access_key = secret_key)
-        conn = S3Connection('____________________', '________________________________________')
         bucket = conn.get_bucket('bucketname')
         key = bucket.get_key("picture.jpg")
         fp = open ("picture.jpg", "w")
         key.get_file (fp)
         key = bucket.get_key('perl_poetry.pdf')
         key.get_contents_to_filename('/home/larry/documents/perl_poetry.pdf')
+
+    def iam2creds(self):
+        return ('yo','lo')
+
+    def env2creds(self):
+        try:
+            access_key = os.environ['AWS_ACCESS_KEY']
+            secret_key = os.environ['AWS_SECRET_KEY']
+            region = os.environ['AWS_REGION']
+        except:
+            print "Environment not set properly!"
+            sys.exit(1)
+        return (access_key, secret_key)
+
     def unpack(self)
         pass
-    def 
-  
 
 def main():
     arguments = docopt.docopt(__doc__)
     if arguments['--debug']:
         print arguments
     repo_url = arguments['--repourl']
-    if arguments['--iam']:
-        repo = s3ymrepo('iam', '') 
-    if arguments['--env']:
-
-
+    repo = 
 
 
 if __name__ == '__main__':
